0a72d9cb70ce3336216715e0587e2cfd350f594e
akashp69 <akashpakhare499@gmail.com>
2022-10-15 00:01:14 +0530

    Added:Program for Swap Two Nibbles

---
+++ src/com/bridgelabz/programsforjunit/SwapTwoNibbles.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.programsforjunit;
+
+import java.util.Arrays;
+
+public class SwapTwoNibbles {
+    public static void main(String[] args) {
+
+        int arr [] = {0,1,1,0,0,1,0,0};
+        int temp,index;
+        index=(arr.length-4);
+         for(int i=0;i<4;i++) {
+             temp       = arr[i];
+             arr[i]     = arr[index];
+             arr[index] = temp;
+             index++;
+
+         }
+         int arr1[]=arr;
+        System.out.println((arr1));
+
+
+    }
+}
